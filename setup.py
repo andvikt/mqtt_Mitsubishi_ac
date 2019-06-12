31d49f892d8d6879d4ad61eecbf13380d1ed5ca5
Andrey <andvikt@gmail.com>
2019-06-12 14:51:01 +0300

    dadad

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ac',
+    version='',
+    packages=find_packages(),
+    include_package_data=True,
+    url='',
+    license='',
+    author='andrewgermanovich',
+    author_email='',
+    description='',
+    install_requires=['pyyaml', 'paho-mqtt', 'click']
+)
